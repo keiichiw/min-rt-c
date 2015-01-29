d342a959a8985e3d686695118ae0edb867bf3c3e
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-01-29 23:14:42 +0900

    Add functions till line 664 in min-rt.ml
    Add read_screen_settings, read_light, rotate_quadratic_matrix, read_nth_object

--- min-rt.c
+++ min-rt.c
@@ -63,6 +63,16 @@ typedef struct {
   float br;
 } refl;
 
+/*
+  global variables
+ */
+
+vec screen;
+vec screenx_dir, screeny_dir, screenz_dir;
+vec viewpoint;
+vec light, beam;
+
+obj *objects; 
 
 /******************************************************************************
    ユーティリティー
@@ -411,4 +421,164 @@ float r_bright (refl *r) {
 /* ラジアン */
 float rad (float x) {
   return x * 0.017453293;
+}
+
+/**** 環境データの読み込み ****/
+
+void read_screen_settings (void) {
+  screen.x = read_float();
+  screen.y = read_float();
+  screen.z = read_float();
+  float v1 = rad(read_float());
+  float cos_v1 = cos(v1);
+  float sin_v1 = sin(v1);
+  float v2 = rad(read_float());
+  float cos_v2 = cos(v2);
+  float sin_v2 = sin(v2);
+  screenz_dir.x = cos_v1 * sin_v2 * 200.0;
+  screenz_dir.y = sin_v1 * (-200.0);
+  screenz_dir.z = cos_v1 * cos_v2 * 200.0;
+  screenx_dir.x = cos_v2;
+  screenx_dir.y = 0.0;
+  screenx_dir.z = - sin_v2;
+  screeny_dir.x = - sin_v1 * sin_v2;
+  screeny_dir.y = - cos_v1;
+  screeny_dir.z = - sin_v1 * cos_v2;
+  viewpoint.x = screen.x - screenz_dir.x;
+  viewpoint.y = screen.y - screenz_dir.y;
+  viewpoint.z = screen.z - screenz_dir.z;
+}
+
+
+void read_light(void) {
+  int nl = radread_int();
+  float l1 = rad(read_float());
+  float sl1 = sin(l1);
+  light.y = - sl1;
+  float l2 = rad(read_float());
+  float cl1 = cos(l1);
+  float sl2 = sin(l2);
+  light.x = cl1 * sl2;
+  float cl2 = cos(l2);
+  light.z = cl1 * cl2;
+  beam.x = read_float();
+}
+
+void rotate_quadratic_matrix(vec *abc, vec *rot) {
+/* 回転行列の積 R(z)R(y)R(x) を計算する */
+  float cos_x = cos(rot->x);
+  float sin_x = sin(rot->x);
+  float cos_y = cos(rot->y);
+  float sin_y = sin(rot->y);
+  float cos_z = cos(rot->z);
+  float sin_z = sin(rot->z);
+
+  float m00 = cos_y * cos_z;
+  float m01 = sin_x * sin_y * cos_z - cos_x * sin_z;
+  float m02 = cos_x * sin_y * cos_z + sin_x * sin_z;
+
+  float m10 = cos_y * sin_z;
+  float m11 = sin_x * sin_y * sin_z + cos_x * cos_z;
+  float m12 = cos_x * sin_y * sin_z - sin_x * cos_z;
+
+  float m20 = - sin_y;
+  float m21 = sin_x * cos_y;
+  float m22 = cos_x * cos_y;
+
+  /* a, b, cの元の値をバックアップ */
+  float ao = abc->x;
+  float bo = abc->y;
+  float co = abc->z;
+
+  /* R^t * A * R を計算 */
+
+  /* X^2, Y^2, Z^2成分 */
+  abc->x = ao * fsqr(m00) + bo * fsqr(m10) + co * fsqr(m20);
+  abc->y = ao * fsqr(m01) + bo * fsqr(m11) + co * fsqr(m21);
+  abc->z = ao * fsqr(m02) + bo * fsqr(m12) + co * fsqr(m22);
+
+  /* 回転によって生じた XY, YZ, ZX成分 */
+  rot->x = 2.0 * (ao * m01 * m02 + bo * m11 * m12 + co * m21 * m22);
+  rot->y = 2.0 * (ao * m00 * m02 + bo * m10 * m12 + co * m20 * m22);
+  rot->z = 2.0 * (ao * m00 * m01 + bo * m10 * m11 + co * m20 * m21);
+}
+
+/**** オブジェクト1つのデータの読み込み ****/
+bool read_nth_object(int n) {
+
+  int texture = read_int();
+  if (texture != -1) {
+    int form = read_int ();
+    int refltype = read_int ();
+    int isrot_p = read_int () ;
+
+    vec abc;
+    abc.x = read_float ();
+    abc.y = read_float ();
+    abc.z = read_float ();
+
+    vec xyz;
+    xyz.x = read_float ();
+    xyz.y = read_float ();
+    xyz.z = read_float ();
+
+    int m_invert = fisneg (read_float ());
+
+    vec reflparam;
+    reflparam.x = read_float (); /* diffuse */
+    reflparam.y = read_float (); /* hilight */
+
+    vec color;
+    color.x = read_float ();
+    color.y = read_float ();
+    color.z = read_float (); /* 15 */
+
+    vec rotation;
+    if (isrot_p != 0) {
+	 rotation.x = rad (read_float ());
+	 rotation.y = rad (read_float ());
+	 rotation.z = rad (read_float ());
+    }
+
+    /* パラメータの正規化 */
+
+    /* 注: 下記正規化 (form = 2) 参照 */
+    bool m_invert2 = form == 2 ? True : m_invert;
+    vec4 ctbl;
+    /* ここからあとは abc と rotation しか操作しない。*/
+    obj obj_m =
+      {texture, form, refltype, isrot_p,
+       abc, xyz, /* x-z */
+       m_invert2,
+       reflparam, /* reflection paramater */
+       color, /* color */
+       rotation, /* rotation */
+       ctbl /* constant table */
+      };
+      objects[n] = obj_m;
+
+      if (form == 3) {
+	/* 2次曲面: X,Y,Z サイズから2次形式行列の対角成分へ */
+	float a = abc.x;
+	abc.x = a == 0.0 ? 0.0 : sgn(a) / fsqr(a); /* X^2 成分 */
+	float b = abc.y;
+	abc.y = b == 0.0 ? 0.0 : sgn(b) / fsqr(b); /* Y^2 成分 */
+	float c = abc.z;
+	abc.z = c == 0.0 ? 0.0 : sgn(c) / fsqr(c);  /* Z^2 成分 */
+      }
+      else if (form == 2) {
+	/* 平面: 法線ベクトルを正規化, 極性を負に統一 */
+	vecunit_sgn(&abc, not(m_invert));
+      }
+
+      /* 2次形式行列に回転変換を施す */
+      if (isrot_p != 0) {
+	rotate_quadratic_matrix(&abc, &rotation);
+      }
+
+      return True;
+  }
+  else {
+    return False; /* データの終了 */
+  }
 }
